64ce0bc86dabde6c221349ed171373dde14a69b7
Satish Singh <ss1220177@gmail.com>
2026-02-22 13:15:24 +0530

    Adding

---
+++ app5.js
@@ -0,0 +1,20 @@
+// let color="red";
+// if(color=="red"){
+// console.log("stop light color is red");
+// }
+let size = "xl";
+if(size=== "xl"){
+    console.log("250");
+}
+    else if(size==="l") {
+    console.log("200");
+    }
+else if (size === "m"){
+    console.log("100");
+} else{
+
+    console.log("50");
+}
+
+    
+
