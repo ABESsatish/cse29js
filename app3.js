e61099fba35d70c38e46904cc52139da809f52c2
Satish Singh <ss1220177@gmail.com>
2026-02-22 11:18:05 +0530

    add

---
+++ app3.js
@@ -0,0 +1,9 @@
+console.log("some code before if");
+let age=23;
+if (age>=18){
+    console.log("you can vote");
+}
+console.log("some code after if");
+if(age>20){
+    console.log("you are in 20");
+}
