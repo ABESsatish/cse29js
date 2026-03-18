0c69dc66d05a42ecb3b2b319b915bf29db85a789
Satish Singh <ss1220177@gmail.com>
2026-03-18 14:21:15 +0530

    dom

---
+++ dom.js
@@ -0,0 +1,46 @@
+// const para=document.getElementById("p1");
+// console.log(para);
+// para.innerHTML="This is a paragraph";
+// const paras=document.getElementsByClassName("p1");
+// console.log(paras);
+// paras[0].innerHTML="This is the first paragraph";
+// paras[1].innerHTML="This is the second paragraph";
+// paras[2].innerHTML="This is the third paragraph";
+// const paras2=document.getElementsByTagName("p");
+// console.log(paras2);
+// paras2[3].innerHTML="This is the fourth paragraph";
+// document.querySelector("#p1").innerHTML="This is the first paragraph using query selector";
+// const paras3=document.querySelectorAll(".p1");
+// console.log(paras3);
+// paras3[0].innerHTML="This is the first paragraph using query selector all";
+// paras3[1].innerHTML="This is the second paragraph using query selector all";
+// paras3[2].innerHTML="This is the third paragraph using query selector all";
+// const paras4=document.querySelectorAll("p");
+// const c =paras4[0].getAttribute("class");
+// const i = paras4[0].getAttribute("id");
+// console.log(c);
+// console.log(i);
+// const heading=document.createElement("h2");
+// const bodytag=document.getElementsByTagName("body")[0];
+// heading.innerHTML="This is a heading";
+// bodytag.appendChild(heading);
+// bodytag[0].prependChild(heading);
+
+function onClick(){
+    console.log("Button clicked");}
+    function onMouseOver(){
+        console.log("Mouse over");}
+        function onMouseOut(){
+            console.log("Mouse out");}
+            function onMouseDown(){
+                console.log("Mouse down");}
+                    function onMouseUp(){
+                        console.log("Mouse up");}
+                        function onDblClick(){
+                            console.log("Double click");}
+
+                            const x=document.querySelector("input");
+                            console.log(x);
+                            
+                        
+
